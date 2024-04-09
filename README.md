f10166e3f28ac3abe3b398cc06a7a3d3b4b441c4
Denys Filichkin <denys.philichkin@gmail.com>
2024-04-09 18:05:26 +0300

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
+<!-- @format -->
+
 # scrapper_bot
+
+Scrapper Bot - это бот Discord, предназначенный для скрапинга данных с веб-сайтов, обработки CSV файлов, взаимодействия с API сервисами и сохранения данных в базе данных PostgreSQL.
+
+## Установка
+
+1. Клонируйте репозиторий на локальную машину:
+
+   ```bash
+   git clone https://github.com/your_username/scrapper_bot.git
+   ```
+
+2. Установите зависимости, используя pip:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Использование
+
+1. Убедитесь, что вы настроили все необходимые параметры в файле `.env`, такие как токены для доступа к сервисам и параметры подключения к базе данных.
+
+2. Запустите приложение с помощью Docker Compose:
+
+   ```bash
+   docker-compose up --build
+   ```
+
+3. Бот будет доступен на вашем сервере Discord по заданному префиксу.
+
+## Структура проекта
+
+scrapper_bot/
+│
+├── bot/
+│
+│ ├── db/
+│ │ ├── connection.py
+│ │ └── execute.py
+│ │
+│ ├── handlers/
+│ │ ├── bot_handler.py
+│ │ ├── api_handler.py
+│ │ └── csv_handler.py
+│ │
+│ ├── log/
+│ │ └── logger.py
+│ │
+│ ├── settings/
+│ │ └── settings.py
+│ │
+│ ├── settings/
+│ │ └── settings.py
+│ │
+│ ├── utils/
+│ │ ├── captcha/
+│ │ └── chromedriver-win64/
+│ │
+│ ├── scraper.py
+│ └── main.py
+│
+├── .env
+│
+├── .gitignore
+│
+├── Dockerfile
+│
+├── docker-compose.yaml
+│
+├── LICENSE
+│
+├── README.md
+│
+└── requirements.txt
+
+[MIT License](LICENSE)
--- bot/scraper.py
+++ bot/scraper.py
@@ -1,8 +1,14 @@
+import time
 import requests
 from zenrows import ZenRowsClient
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
 from bot.settings.settings import settings
 
+# use ZenRows
 url = settings.URL
 api_key = settings.ZR_KEY
 
@@ -25,3 +31,48 @@ def register_user(email, password, confirm_password):  # email, password, confir
         password (str): User's password.
         confirm_password (str): Confirmation of user's password.
     """
+   
+# use selenium
+
+options = webdriver.ChromeOptions()
+service = Service(executable_path="/bot/utils/chromedriver-win64/chromedriver.exe")
+
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    'source': '''
+         delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array;
+         delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise;
+         delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol;
+         delete window.cdc_adoQpoasnfa76pfcZLmcfl_JSON;
+         delete window.cdc_adoQpoasnfa76pfcZLmcfl_Object;
+         delete window.cdc_adoQpoasnfa76pfcZLmcfl_Proxy;
+    '''
+})
+
+def register_user(email, password, confirm_password): 
+    try:
+        driver.get("https://go.seated.com/tour-events/ee8effe7-d205-403c-a39f-007a83ac9629")
+        # TODO rewrite logical func
+        # email_input = driver.find_element(By.NAME, "email")
+        # email_input.send_keys(email)
+
+        # password_input = driver.find_element(By.NAME, "password")
+        # password_input.send_keys(password)
+
+        # confirm_password_input = driver.find_element(By.NAME, "passwordConfirm")
+        # confirm_password_input.send_keys(confirm_password)
+
+        success_message = driver.find_elements(By.XPATH, "//div[contains(text(), 'Successfully registered')]")   
+        time.sleep(80)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        if success_message:
+            driver.quit()
+            return True
+        else:
+            driver.quit()
+            return False
